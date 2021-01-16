91c006cacfde5cf8bdae692fc71464cceb3fdf43
mhaghpanah <40441978+mhaghpanah@users.noreply.github.com>
2021-01-16 14:21:47 -0600

    Update PlanarGraph.java

--- src/main/java/OrderTypeGraph/PlanarGraph.java
+++ src/main/java/OrderTypeGraph/PlanarGraph.java
@@ -5,9 +5,14 @@ public class PlanarGraph extends Graph {
   Integer crossingNumber;
   Points points;
 
+  public PlanarGraph(Points points) {
+    super(points.size());
+    this.points = points;
+  }
+
   public PlanarGraph(Graph graph, Points points) {
-    super(graph.n);
-    this.edges = graph.getEdges();
+    super(graph.edgesNumber());
+    assert points.size() == graph.verticesNumber();
     this.points = points;
   }
 
@@ -20,10 +25,10 @@ public class PlanarGraph extends Graph {
 
   public int compareTo(PlanarGraph that) {
     assert that != null;
-    if (this.size() == that.size()) {
+    if (this.edgesNumber() == that.edgesNumber()) {
       return Integer.compare(this.crossingNumber(), that.crossingNumber());
     }
-    return Integer.compare(this.size(), that.size());
+    return Integer.compare(this.edgesNumber(), that.edgesNumber());
   }
 
 }
