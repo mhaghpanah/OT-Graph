fee4298d1a89d22c810392b32419c46ebbadae11
mhaghpanah <40441978+mhaghpanah@users.noreply.github.com>
2021-01-16 20:49:31 -0600

    Update and rename GraphGeneratorFunctions.java to GraphGeneratorWrapper.java

--- src/main/java/OrderTypeGraph/GraphGeneratorWrapper.java
+++ src/main/java/OrderTypeGraph/GraphGeneratorWrapper.java
@@ -1,14 +1,10 @@
 package OrderTypeGraph;
 
-public class GraphGeneratorFunctions extends GraphGenerator {
-
-  GraphGeneratorFunctions(PreCCSystem goal, boolean[] rules) {
-    super(goal, rules);
-  }
-
-  public static Tuple<Graph, Proofs> generateGraphWithProofs(PreCCSystem goal, int algoType,
+public class GraphGeneratorWrapper {
+  
+  public static Tuple<Graph, Proofs> generateGraphWithProofs(Points points, int algoType,
       boolean[] rules) {
-    GraphGenerator gg = new GraphGenerator(goal, rules);
+    GraphGenerator gg = new GraphGenerator(points, rules);
     Graph graph;
     switch (algoType) {
       case 0:
@@ -24,8 +20,8 @@ public class GraphGeneratorFunctions extends GraphGenerator {
     return new Tuple<>(graph, gg.computeCCClosure(graph).getProofs());
   }
 
-  public static Graph generateGraph(PreCCSystem goal, int algorithmType, boolean[] rules) {
-    GraphGenerator gg = new GraphGenerator(goal, rules);
+  public static Graph generateGraph(Points points, int algorithmType, boolean[] rules) {
+    GraphGenerator gg = new GraphGenerator(points, rules);
     switch (algorithmType) {
       case 0:
         return gg.runBottomUpRandomAlgorithm();
@@ -36,12 +32,11 @@ public class GraphGeneratorFunctions extends GraphGenerator {
     }
   }
 
-  public static Graph generateGraph(PreCCSystem goal, int algorithmType, boolean[] rules,
-      int repeatNum) {
-    int n = goal.getN();
+  public static Graph generateGraph(Points points, int algorithmType, boolean[] rules, int repeatNum) {
+    int n = points.size();
     Graph ans = Graph.completeGraph(n);
     for (int i = 0; i < repeatNum; i++) {
-      Graph graph = generateGraph(goal, algorithmType, rules);
+      Graph graph = generateGraph(points, algorithmType, rules);
       if (graph.getM() < ans.getM()) {
         ans = graph;
       }
@@ -50,12 +45,11 @@ public class GraphGeneratorFunctions extends GraphGenerator {
     return ans;
   }
 
-  public static PlanarGraph generateGraph(Points points, int algorithmType, boolean[] rules,
+  public static PlanarGraph generateGraphCrossOrder(Points points, int algorithmType, boolean[] rules,
       int repeatNum) {
-    PreCCSystem goal = points.computeCCSystem();
     PlanarGraph ans = PlanarGraph.completeGraph(points);
     for (int i = 0; i < repeatNum; i++) {
-      Graph graph = generateGraph(goal, algorithmType, rules);
+      Graph graph = generateGraph(points, algorithmType, rules);
       PlanarGraph planarGraph = new PlanarGraph(graph, points);
       if (planarGraph.compareTo(ans) < 0) {
         ans = planarGraph;
@@ -65,10 +59,10 @@ public class GraphGeneratorFunctions extends GraphGenerator {
     return ans;
   }
 
-  public static int generateGraphIterationCalc(PreCCSystem goal, int algorithmType, boolean[] rules,
+  public static int generateGraphIterationCalc(Points points, int algorithmType, boolean[] rules,
       int desireEdgeNum, int maxRepeatNum) {
     for (int i = 0; i < maxRepeatNum; i++) {
-      Graph graph = generateGraph(goal, algorithmType, rules);
+      Graph graph = generateGraph(points, algorithmType, rules);
       if (graph.getM() <= desireEdgeNum) {
         return i + 1;
       }
