b801244bec07c2cf38a2a7c0985070ac41c4ec35
mhaghpanah <40441978+mhaghpanah@users.noreply.github.com>
2021-02-16 00:44:46 -0600

    Update and rename PlanarGraph.java to PlaneGraph.java

--- src/main/java/OrderTypeGraph/PlaneGraph.java
+++ src/main/java/OrderTypeGraph/PlaneGraph.java
@@ -1,25 +1,25 @@
 package OrderTypeGraph;
 
-public class PlanarGraph extends Graph {
+public class PlaneGraph extends Graph {
 
   Integer crossingNumber;
   private final Points points;
 
-  public PlanarGraph(Points points) {
+  public PlaneGraph(Points points) {
     super(points.size());
     this.points = points;
   }
 
-  public PlanarGraph(Graph graph, Points points) {
+  public PlaneGraph(Graph graph, Points points) {
     super(graph);
     assert points.size() == graph.getN();
     this.points = points;
   }
 
-  public static PlanarGraph completeGraph(Points points) {
+  public static PlaneGraph completeGraph(Points points) {
     int n = points.size();
     Graph completeGraph = Graph.completeGraph(n);
-    return new PlanarGraph(completeGraph, points);
+    return new PlaneGraph(completeGraph, points);
   }
 
   public Points getPoints() {
@@ -33,7 +33,7 @@ public class PlanarGraph extends Graph {
     return crossingNumber;
   }
 
-  public int compareTo(PlanarGraph that) {
+  public int compareTo(PlaneGraph that) {
     assert that != null;
     if (this.getM() == that.getM()) {
       return Integer.compare(this.crossingNumber(), that.crossingNumber());
