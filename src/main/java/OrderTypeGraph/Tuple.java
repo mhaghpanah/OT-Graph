3967ba6a2a498dc1bd0f0f130534a74e6171b2b6
mhaghpanah <40441978+mhaghpanah@users.noreply.github.com>
2021-02-16 00:52:09 -0600

    Update Tuple.java

--- src/main/java/OrderTypeGraph/Tuple.java
+++ src/main/java/OrderTypeGraph/Tuple.java
@@ -1,11 +1,11 @@
 package OrderTypeGraph;
 
-public class Tuple<X, Y> {
+public class Pair<X, Y> {
 
   public final X x;
   public final Y y;
 
-  public Tuple(X x, Y y) {
+  public Pair(X x, Y y) {
     this.x = x;
     this.y = y;
   }
